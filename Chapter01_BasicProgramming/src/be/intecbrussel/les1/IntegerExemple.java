f2cc6b6e059546ea96aaa2647fbd26b699dccd93
alekseylee <110702662+alekseylee@users.noreply.github.com>
2023-09-19 15:51:25 +0200

    Initial commit

---
+++ Chapter01_BasicProgramming/src/be/intecbrussel/les1/IntegerExemple.java
@@ -0,0 +1,16 @@
+package be.intecbrussel.les1;
+
+public class IntegerExemple {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println("De waarde van variabel a=" + a);
+        System.out.println("De waarde van variabel b=" + b);
+        System.out.println("the sum of a and b is =" + c);
+
+
+    }
+}
---
+++ Chapter01_BasicProgramming/src/be/intecbrussel/les1/ShortExemple.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.les1;
+
+public class ShortExemple {
+    public static void main(String[] args) {
+        short a = 10;
+        short b = -20;
+        System.out.println("De waarde van variabel a=" + a);
+        System.out.println("De waarde van variabel b=" + b);
+
+
+    }
+}
