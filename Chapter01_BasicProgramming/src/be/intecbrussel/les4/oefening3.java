7851d1549d5d05fb73d60443df752ad1df4db5de
Haytam Aroui <haytam.aroui@gmail.com>
2023-09-20 15:37:55 +0200

    update les 3 and 4

---
+++ Chapter01_BasicProgramming/src/be/intecbrussel/les4/oefening3.java
@@ -0,0 +1,44 @@
+/*package be.intecbrussel.les4;
+
+public class oefening3 {
+    public static void main(String[] args) {
+        String month = "Maart";
+        int daysCount = 0;
+
+        switch (month) {
+            case "januari":
+            case "maart":
+            case "mei":
+            case "juli":
+            case "augustus":
+            case "october":
+            case "november":
+                int daysCount=31;
+                if (month == daysCount)
+                    System.out.println("Het aantal dagen in "+month +"is" +daysCount);
+
+                break;
+            case "april":
+            case "juni":
+            case "september":
+            case "december":
+                int daysCount=30;
+                if (month == daysCount)
+                    System.out.println("Het aantal dagen in "+month +"is" +daysCount);
+
+                break;
+
+            case "februari":
+                int daysCount=28;
+                if (month == daysCount)
+                    System.out.println("Het aantal dagen in "+month +"is" +daysCount);
+
+
+            default:
+                System.out.println("ongieldige maad");
+
+
+        }
+
+    }
+}*/
