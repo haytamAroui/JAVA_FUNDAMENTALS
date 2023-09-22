7c1ca1a8476bac7c4e35cb4afd941af6906b0912
Haytam Aroui <haytam.aroui@gmail.com>
2023-09-22 13:54:54 +0200

    Delete Chapter01_BasicProgramming/src/be/intecbrussel/les4 directory

--- Chapter01_BasicProgramming/src/be/intecbrussel/les4/les1/MyFirstProject.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.les1;
-
-public class MyFirstProject {
-
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-
-}
--- Chapter01_BasicProgramming/src/be/intecbrussel/les4/les2/oefening1.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.les2;
-
-public class oefening1 {
-
-    public static void main(String[] args) {
-        int a=20;
-        System.out.println(a);
-    }
-}
