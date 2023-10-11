939fbba021bdf999e92ac8740e4682f37a4d2123
Haytam Aroui <haytam.aroui@gmail.com>
2023-10-11 16:08:02 +0200

    Update Circle.java

--- Chapter03_Classes/src/Opdracht4/Circle.java
+++ Chapter03_Classes/src/Opdracht4/Circle.java
@@ -1,68 +1,78 @@
-package be.intecbrussel.les1.Opdracht4;
+package be.intecbrussel.les1.Opdracht4;public class Circle {
 
-    public class Circle {
-        private int x;
-        private int y;
-        private int radius;
+    public static final int ANGLES = 0;
 
-        public Circle() {
-            this.radius = 0;
-            this.x = 0;
-            this.y = 0;
-        }
+    private int count;
+    private int x;
+    private int y;
+    private int radius;
 
-        public Circle(int radius) {
-            this.radius = radius;
-            this.x = 0;
-            this.y = 0;
-        }
-
-        public Circle(int radius, int x, int y) {
-            this.radius = radius;
-            this.x = x;
-            this.y = y;
-        }
-
-        public Circle(Circle c) {
-            this.radius = c.radius;
-            this.x = c.x;
-            this.y = c.y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
-        public int getRadius() {
-            return radius;
-        }
-
-        public void setRadius(int radius) {
-            this.radius = radius;
-        }
-
-        public double getArea() {
-            return 3.14 * (this.radius * this.radius);
-        }
-
-        public double getPerimeter() {
-            return 2 * 3.14 * this.radius;
-        }
-
-        public void grow(int d) {
-            this.radius += d;
-        }
+    public Circle() {
+        this.count = 0;
+        this.x = 0;
+        this.y = 0;
+        this.radius = 0;
     }
 
+    public Circle(int radius) {
+        this.count = 0;
+        this.x = 0;
+        this.y = 0;
+        this.radius = radius;
+    }
+
+    public Circle(int radius, int x, int y) {
+        this.count = 0;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Circle(Circle c) {
+        this.count = c.count;
+        this.x = c.x;
+        this.y = c.y;
+        this.radius = c.radius;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    public void grow(int d) {
+        this.radius += d;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+}
